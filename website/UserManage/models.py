2bb9759d7e604a64988d4eb30a46e489ce1a459b
chaos <497386856@qq.com>
2016-11-12 21:56:02 +0800

    add a home page

--- website/UserManage/models.py
+++ website/UserManage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(primary_key=True,max_length=20)
+    is_active = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    nickname = models.CharField(max_length=64, null=True)
+    openid = models.CharField(max_length=100,unique=True)
