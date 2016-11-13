236fe64cf1776ebce44ea016aad55704165b9be2
chaos <497386856@qq.com>
2016-11-13 16:19:21 +0800

    start UserManage.models

--- website/UserManage/models.py
+++ website/UserManage/models.py
@@ -8,3 +8,18 @@ class User(AbstractBaseUser):
     is_superuser = models.BooleanField(default=False)
     nickname = models.CharField(max_length=64, null=True)
     openid = models.CharField(max_length=100,unique=True)
+    qq_num = models.CharField(max_length=30,unique=True)
+
+
+class Sender(User):
+    pass
+
+
+
+class Taker(User):
+    pass
+
+
+class Order(models.Model):
+    order_id = models.CharField(max_length=30,primary_key=True)
+    original_file = models.FileField()
